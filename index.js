c2dcb7a574a89f2f749e6486e6b0b3d69734a2d2
Juan Carlos Fernandez <juanf1986@gmail.com>
2023-05-04 21:53:14 +0200

    Ejercicio API book CRUD

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { bookRouter } = require("./routes/book.routes.js");
+
+const { connect } = require("./db.js");
+connect();
+
+const PORT = 3000;
+const server = express();
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+
+const router = express.Router();
+router.get("/", (req, res) => {
+  res.send("Esta es la home de nuestra API");
+});
+router.get("*", (req, res) => {
+  res.status(404).send("Lo sentimos :( No hemos encontrado la página solicitada.");
+});
+
+server.use("/book", bookRouter);
+server.use("/", router);
+
+server.listen(PORT, () => {
+  console.log(`Server levantado en el puerto ${PORT}`);
+});
